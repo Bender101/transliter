49c023885b7f427b53ce96c05b1b22e37e536bb2
Bender101 <serkok227@gmail.com>
2021-12-31 15:07:43 +0300

    Release 5 done

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const button = document.getElementById('button');
 const inputText = document.getElementById('input0');
+const blockRu = document.getElementById('blockRu');
+const blockTrans = document.getElementById('blockTrans');
 
 button.addEventListener('click', () => {
     const text = inputText.value;
     console.log(text);
+    const blockWords = document.createElement('div');
+    blockRu.appendChild(blockWords);
+    blockWords.innerText = text;
 });
